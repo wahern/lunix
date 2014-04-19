1016cb9d0bc00cf650493dcdc8fbf59522dd8301
william <william@25tandclement.com>
2014-04-19 15:54:39 -0700

    add readdir example

---
+++ examples/readdir.lua
@@ -0,0 +1,34 @@
+#!/usr/bin/env lua
+
+local unix = require"unix"
+
+local tmap = {
+	b = unix.S_ISBLK,
+	c = unix.S_ISCHR,
+	d = unix.S_ISDIR,
+	l = unix.S_ISLNK,
+	s = unix.S_ISSOCK,
+	p = unix.S_ISFIFO,
+	f = unix.S_ISREG,
+}
+
+local function tname(mode)
+	for name, f in pairs(tmap) do
+		if f(mode) then
+			return name
+		end
+	end
+
+	return "-"
+end -- tname
+
+
+local tmp = unix.opendir(io.open(... or ".", "r"))
+local dir = unix.opendir(tmp) --> can open from existing handle
+tmp:close()
+
+for type, name in dir:files("type", "name") do
+	print(tname(type), name)
+end
+
+dir:close()
