ae2a9965a356c06846daed72936316dd4b116ab7
william <william@25tandclement.com>
2014-08-01 17:00:41 -0700

    add flock example

---
+++ examples/flock.lua
@@ -0,0 +1,19 @@
+-- POSIX fcntl file locks, not BSD flock
+
+local unix = require"unix"
+
+local fh = assert(io.tmpfile())
+
+assert(unix.fcntl(fh, unix.F_SETLK))
+io.stdout:write(string.format("%d has lock\n", unix.getpid()))
+
+io.stdout:write(string.format("forking\n"))
+
+if 0 == unix.fork() then
+	local l = assert(unix.fcntl(fh, unix.F_GETLK))
+	assert(unix.getppid() == l.pid)
+	io.stdout:write(string.format("F_GETLK from %d confirms %d has file\n", unix.getpid(), l.pid))
+else
+	assert(unix.waitpid())
+end
+
