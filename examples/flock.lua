55beaf781f13d25be74a98f8410b4a0eb1856e20
william <william@25tandclement.com>
2014-08-01 17:02:47 -0700

    fix messages in flock.lua

--- examples/flock.lua
+++ examples/flock.lua
@@ -5,14 +5,14 @@ local unix = require"unix"
 local fh = assert(io.tmpfile())
 
 assert(unix.fcntl(fh, unix.F_SETLK))
-io.stdout:write(string.format("%d has lock\n", unix.getpid()))
+io.stdout:write(string.format("%d has file lock\n", unix.getpid()))
 
 io.stdout:write(string.format("forking\n"))
 
 if 0 == unix.fork() then
 	local l = assert(unix.fcntl(fh, unix.F_GETLK))
 	assert(unix.getppid() == l.pid)
-	io.stdout:write(string.format("F_GETLK from %d confirms %d has file\n", unix.getpid(), l.pid))
+	io.stdout:write(string.format("F_GETLK from %d confirms %d has file lock\n", unix.getpid(), l.pid))
 else
 	assert(unix.waitpid())
 end
