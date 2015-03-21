c736d1cd9fe50da717be5fbc87e51414b34ea8d6
william <william@25thandclement.com>
2015-03-20 19:05:22 -0700

    add regression test for fopen, and fopen on LuaJIT in particular because of issues with creating new files handles

---
+++ regress/fopen-luajit.lua
@@ -0,0 +1,24 @@
+#!/bin/sh
+_=[[
+	. "${0%%/*}/regress.sh"
+	exec runlua -j- "$0" "$@"
+]]
+
+require"regress".export".*"
+local unix = require"unix"
+
+local dt = os.date()
+local fh = check(io.tmpfile())
+
+check(fh:write(dt, "\n"))
+check(fh:flush())
+
+local fh2 = check(unix.fopen(fh, unix.O_CLOEXEC))
+local ln = check(fh2:seek"set") and check(fh2:read())
+
+check(dt == ln, "could not read back line (%s ~= %s)", dt, tostring(ln))
+check(unix.fileno(fh) ~= unix.fileno(fh2), "descriptor numbers not different")
+check(unix.O_CLOEXEC == unix.bitand(unix.fcntl(fh2, unix.F_GETFL), unix.O_CLOEXEC), "O_CLOEXEC flag not set")
+
+
+say"OK"
